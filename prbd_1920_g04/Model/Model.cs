17a2715bca0fd5d02f61bdd42f4480ad4e264637
Kevin David L. Girs <kevin.girs@outlook.com>
2020-04-18 02:34:31 +0200

    Renommage du projet xyy -> G04

---
+++ prbd_1920_g04/Model/Model.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using PRBD_Framework;
+
+namespace prbd_1920_g04
+{
+    public enum Role {
+        Member = 1,
+        Admin = 2
+    }
+
+    public class Model : DbContext {
+        public Model() : base("prbd_1920_g04") {
+            // la base de données est supprimée et recréée quand le modèle change
+            Database.SetInitializer<Model>(new DropCreateDatabaseIfModelChanges<Model>());
+        }
+
+        public DbSet<Member> Members { get; set; }
+
+        public Member CreateMember(string pseudo, string password, Role role = Role.Member) {
+            var member = Members.Create();
+            member.Pseudo = pseudo;
+            member.Password = password;
+            member.Role = role;
+            Members.Add(member);
+            return member;
+        }
+
+        public void SeedData() {
+            if (Members.Count() == 0) {
+                Console.Write("Seeding members... ");
+                var admin = CreateMember("admin", "admin", Role.Admin);
+                var ben = CreateMember("ben", "ben");
+                var bruno = CreateMember("bruno", "bruno");
+                SaveChanges();
+                Console.WriteLine("ok");
+            }
+        }
+    }
+}
---
+++ prbd_1920_g04/Views/MemberDetailView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PRBD_Framework;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Input;
+
+namespace prbd_1920_g04
+{
+    public partial class MemberDetailView : UserControlBase {
+
+        public Member Member { get; set; }
+
+        public MemberDetailView(Member member) {
+            InitializeComponent();
+            DataContext = this;
+            Member = member;
+        }
+    }
+}
