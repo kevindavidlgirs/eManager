b9f4e77154acdf8365f0374c7720480935dd5732
Herve <herve.mbilo@gmail.com>
2020-05-27 22:35:28 +0200

    Mise à jour dev

--- prbd_1920_g04/Views/MainView.xaml.cs
+++ prbd_1920_g04/Views/MainView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PRBD_Framework;
+using System;
 using System.Collections.Generic;
+using System.Data.Linq;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 namespace prbd_1920_g04.Views {
-    public partial class MainView : Window {
+    public partial class MainView : WindowBase {
         public MainView() {
             InitializeComponent();
             DataContext = this;
@@ -86,34 +88,38 @@ namespace prbd_1920_g04.Views {
             });
 
         }
-          /*  
-            private void tabOfMember(Model.Match m, bool isNew) {
-                foreach (TabItem t in tabControl.Items) {
-                    if (t.Header.ToString().Equals(m.DateMatch)) {
-                        Dispatcher.InvokeAsync(() => t.Focus());
-                        return;
-                    }
-                }
-                var tab = new TabItem() {
-                    Header = isNew ? "<new match>" : m.Adversary,
-                    Content = new MatchDetailView(m, isNew)
-                };
-                tabControl.Items.Add(tab);
-                tab.MouseDown += (o, e) => {
-                    if (e.ChangedButton == MouseButton.Middle &&
-                        e.ButtonState == MouseButtonState.Pressed) {
-                        tabControl.Items.Remove(o);
-                        (tab.Content as UserControlBase).Dispose();
-                    }
-                };
-                tab.PreviewKeyDown += (o, e) => {
-                    if (e.Key == Key.W && Keyboard.IsKeyDown(Key.LeftCtrl)) {
-                        tabControl.Items.Remove(o);
-                        (tab.Content as UserControlBase).Dispose();
-                    }
-                };
-                // exécute la méthode Focus() de l'onglet pour lui donner le focus (càd l'activer)
-                Dispatcher.InvokeAsync(() => tab.Focus());
-            }*/
+
+        private void MatchsView_Loaded(object sender, RoutedEventArgs e) {
+
         }
+        /*  
+ private void tabOfMember(Model.Match m, bool isNew) {
+     foreach (TabItem t in tabControl.Items) {
+         if (t.Header.ToString().Equals(m.DateMatch)) {
+             Dispatcher.InvokeAsync(() => t.Focus());
+             return;
+         }
+     }
+     var tab = new TabItem() {
+         Header = isNew ? "<new match>" : m.Adversary,
+         Content = new MatchDetailView(m, isNew)
+     };
+     tabControl.Items.Add(tab);
+     tab.MouseDown += (o, e) => {
+         if (e.ChangedButton == MouseButton.Middle &&
+             e.ButtonState == MouseButtonState.Pressed) {
+             tabControl.Items.Remove(o);
+             (tab.Content as UserControlBase).Dispose();
+         }
+     };
+     tab.PreviewKeyDown += (o, e) => {
+         if (e.Key == Key.W && Keyboard.IsKeyDown(Key.LeftCtrl)) {
+             tabControl.Items.Remove(o);
+             (tab.Content as UserControlBase).Dispose();
+         }
+     };
+     // exécute la méthode Focus() de l'onglet pour lui donner le focus (càd l'activer)
+     Dispatcher.InvokeAsync(() => tab.Focus());
+ }*/
+    }
 }
